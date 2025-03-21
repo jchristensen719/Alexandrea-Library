343f05b847f49bda51e26f5889da1e0690f94b8e
jchristensen719 <jchristensen719@gmail.com>
2025-03-21 16:20:19 -0500

    Fix syntax and configuration issues

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+# Read requirements
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
 setup(
-    name="alexandrea-library",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        'setuptools',
-    ],
-    python_requires='>=3.8',
+    name="alexandrea_library",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.8",
+    install_requires=requirements,
+    author="J Christensen",
+    author_email="jchristensen719@github.com",
+    description="Alexandrea Library project - A modern library management system",
+    keywords="library, books, alexandria",
+    url="https://github.com/jchristensen719/Alexandrea-Library",
 )
